6ebafaea06bb9e1afd1cd332f18bd24460b326c5
HassanWalid <hassan.walid77@gmail.com>
2024-07-25 06:13:14 +0300

    file attachement with bugs

--- src/Components/chat.jsx
+++ src/Components/chat.jsx
@@ -15,6 +15,12 @@ import {
   set,
   onChildAdded,
 } from "firebase/database";
+import {
+  getStorage,
+  ref as refStorage,
+  uploadBytes,
+  getDownloadURL,
+} from "firebase/storage";
 import app from "../config.js";
 import img from "../images/logo192.png";
 function Chat({ drawer }) {
@@ -22,13 +28,25 @@ function Chat({ drawer }) {
   const { roomId } = useParams();
   // const [roomId] = useContext(Room);
   const [roomName, setRoomName] = useState("");
-  const [messages, setMessages] = useState([]);
+  const [messagesState, setMessagesState] = useState([]);
   const [currentTime, setTime] = useState("");
   const db = getDatabase(app);
+  const storage = getStorage(app);
   const dbRef = ref(db, "chatRooms");
   const [roomDBId, setRoomDBId] = useState("");
   const [openEmoji, setOpenEmoji] = useState(false);
+  const [files, setFiles] = useState([]);
   const bottomRef = useRef(null);
+  const textAreaRef = useRef(null);
+  const handleAttachFile = (e) => {
+    console.log("ee=", e);
+    console.log("ref", textAreaRef);
+    for (let item of e.target.files) {
+      textAreaRef.current.value += item.name + "\n";
+      setFiles([...files, item]);
+      console.log("item", item.name);
+    }
+  };
 
   useEffect(() => {
     if (roomId) {
@@ -46,9 +64,9 @@ function Chat({ drawer }) {
             setRoomName(roomData.roomName);
             if (roomData.messages) {
               // console.log(roomData.messages);
-              setMessages(roomData.messages);
+              setMessagesState(roomData.messages);
             } else {
-              setMessages([]);
+              setMessagesState([]);
             }
           } else {
             console.log("false");
@@ -65,10 +83,10 @@ function Chat({ drawer }) {
   }, [input]);
 
   useEffect(() => {
-    setMessages([]);
+    setMessagesState([]);
     const dbRefMessages = ref(db, `chatRooms/${roomDBId}/messages`);
     const unsubscribe = onChildAdded(dbRefMessages, (snapshot) => {
-      setMessages((prevMessages) => {
+      setMessagesState((prevMessages) => {
         // console.log("prev", prevMessages);
         console.log("snap", snapshot.val());
         return [...prevMessages, snapshot.val()];
@@ -81,27 +99,54 @@ function Chat({ drawer }) {
 
   const sendMessage = async (e) => {
     // e.preventDefault();
+
     console.log("ads=", roomDBId);
     const dbRefMessages = ref(db, `chatRooms/${roomDBId}/messages`);
+    let newMessageObject;
     const snapshot = await get(dbRefMessages);
     let messages = snapshot.val() || [];
+    if (files.length > 0) {
+      const storageRef = refStorage(storage, `attachments/${files[0].name}`);
+      await uploadBytes(storageRef, files[0]);
+      const url = await getDownloadURL(storageRef);
 
-    const newMessageObject = {
-      content: input,
-      username: "username",
-      date: new Date().toLocaleString("en-US", {
-        month: "short",
-        day: "numeric",
-        year: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        hour12: true,
-      }),
-    };
+      newMessageObject = {
+        content: {
+          type: files[0].type.startsWith("image/") ? "image" : "file",
+          url: url,
+          name: files[0].name,
+        },
+        username: "username",
+        date: new Date().toLocaleString("en-US", {
+          month: "short",
+          day: "numeric",
+          year: "numeric",
+          hour: "numeric",
+          minute: "numeric",
+          hour12: true,
+        }),
+      };
+    } else {
+      newMessageObject = {
+        content: input,
+        username: "username",
+        date: new Date().toLocaleString("en-US", {
+          month: "short",
+          day: "numeric",
+          year: "numeric",
+          hour: "numeric",
+          minute: "numeric",
+          hour12: true,
+        }),
+      };
+    }
+
     messages.push(newMessageObject);
-    // setMessages(messages);
+    // setMessagesState(messages);
     await set(dbRefMessages, messages);
     setInput("");
+    setFiles([]);
+    textAreaRef.current.value = "";
     window.scrollTo({
       top: document.documentElement.scrollHeight,
       behavior: "smooth",
@@ -124,7 +169,7 @@ function Chat({ drawer }) {
       <div className="chatbody">
         {/* {console.log(typeof messages)} */}
 
-        {messages.map((message) => (
+        {messagesState.map((message) => (
           <div
             className={
               message.username === "username" ? "chat_message" : "chat_reciver"
@@ -155,7 +200,23 @@ function Chat({ drawer }) {
                     : { backgroundColor: "lightgrey" }
                 }
               >
-                <pre>{message.content}</pre>
+                {typeof message.content === "string" ? (
+                  <pre>{message.content}</pre>
+                ) : message.content.type === "image" ? (
+                  <img
+                    src={message.content.url}
+                    alt={message.content.name}
+                    width={200}
+                  ></img>
+                ) : (
+                  <a
+                    href={message.content.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {message.content.name}
+                  </a>
+                )}
               </div>
               <span className="timeMessage">{message.date}</span>
             </div>
@@ -182,6 +243,7 @@ function Chat({ drawer }) {
         </div>
         <form>
           <textarea
+            ref={textAreaRef}
             style={{
               borderRadius: "15px",
               resize: "none",
@@ -200,10 +262,18 @@ function Chat({ drawer }) {
             onKeyDown={handlePressEnter}
           />
         </form>
-
         <IconButton>
-          <AttachFileIcon />
+          <label htmlFor="fileUpload" style={{ cursor: "pointer" }}>
+            <AttachFileIcon />
+          </label>
         </IconButton>
+        <input
+          type="file"
+          id="fileUpload"
+          style={{ display: "none" }}
+          onChange={handleAttachFile}
+          multiple
+        />
       </div>
     </div>
   );
--- src/config.js
+++ src/config.js
@@ -8,14 +8,14 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: "AIzaSyC-xbBM5BS2dPy6A8CtcEmDhb9kPRQruOs",
   authDomain: "chat-system-6ad6a.firebaseapp.com",
-  DatabaseURL:"https://chat-system-6ad6a-default-rtdb.firebaseio.com",
+  DatabaseURL: "https://chat-system-6ad6a-default-rtdb.firebaseio.com",
   projectId: "chat-system-6ad6a",
   storageBucket: "chat-system-6ad6a.appspot.com",
   messagingSenderId: "1097554807023",
-  appId: "1:1097554807023:web:ab721436a4f488bff1f31a"
+  appId: "1:1097554807023:web:ab721436a4f488bff1f31a",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export default app
+export default app;
